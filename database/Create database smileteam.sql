d9168cedec13ed8d277feac476d0f180659675da
enzo-arg <enzo.argenal@unah.hn>
2026-07-21 22:52:19 -0600

    Creacion DataBase

---
+++ database/Create database smileteam.sql
@@ -0,0 +1,14 @@
+-- Database: SmileTeam
+
+-- DROP DATABASE IF EXISTS "SmileTeam";
+
+CREATE DATABASE "SmileTeam"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Honduras.1252'
+    LC_CTYPE = 'Spanish_Honduras.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
